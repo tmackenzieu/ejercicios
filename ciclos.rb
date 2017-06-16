868b66c3a82b93e71e4039a23bca594775e65d89
Tabata G. Mackenzie <tmackenzieu@gmail.com>
2017-06-15 23:33:43 -0400

    add multiples examples of iteradores

--- ciclos.rb
+++ ciclos.rb
@@ -14,3 +14,29 @@ until contador > 10
   puts contador
   contador = contador + 1
 end
+
+#Iteradores
+	#Loop
+
+i = 20
+loop do
+  i -= 1
+  print "#{i}"
+  break if i <= 0
+end
+	#Next 
+
+i = 20
+loop do
+  i -= 1
+  next if i % 2 != 0
+  print "#{i}"
+  break if i <= 0
+end
+	#Each
+array = [1,2,3,4,5]
+
+array.each do |x|
+  x += 10
+  print "#{x}"
+end
