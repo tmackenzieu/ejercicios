d62e1acd7300b6a1acf1404bc7740cc1f8686027
Tabata G. Mackenzie <tmackenzieu@gmail.com>
2017-06-15 22:53:45 -0400

    add bucle until

--- ciclos.rb
+++ ciclos.rb
@@ -1,9 +1,16 @@
+#Bucles
 for contador in 1..20
     puts contador
 end
 
 contador = 1
 while contador < 11
+  puts contador
+  contador = contador + 1
+end
+
+contador = 1
+until contador > 10
   puts contador
   contador = contador + 1
 end
