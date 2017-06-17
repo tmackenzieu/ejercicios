050d69ad6fa4de78d0cb19aa05e1b68a85022277
Tabata G. Mackenzie <tmackenzieu@gmail.com>
2017-06-17 00:11:58 -0400

    add convert  to uppercase the strings

--- modificarPalabras.rb
+++ modificarPalabras.rb
@@ -1,8 +1,10 @@
 puts " Ingresa un texto por favor"
 texto = gets.chomp
+texto.upcase!
 
 puts " Ingresa la palabra a modificar"
 modificar = gets.chomp
+modificar.upcase!
 
 palabras = texto.split(" ")
 
