c0391818625eeb20bfd040f79e4d13272a327808
Tabata G. Mackenzie <tmackenzieu@gmail.com>
2017-07-02 23:23:03 -0400

    ordenamiento a la inversa

--- ordenamiento.rb
+++ ordenamiento.rb
@@ -8,3 +8,14 @@ array.each do |item|
   puts "Imprime el elemento #{item}"
 end
 
+=begin
+	Usa .sort! para clasificar el arreglo frutas en orden alfabético descendente (es decir, a la inversa). 
+	Puedes usar el operador combinado de comparación o una sentencia if/elsif/else.
+=end
+
+
+frutas = ["uvas", "pera", "naranja", "manzana", "banano"]
+
+frutas.sort! { |primeraFruta, segundaFruta| primeraFruta <=> segundaFruta }
+
+frutas.reverse!
